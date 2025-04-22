9a18d09e8541af501b790de03dae64cc0f0acc24
Anderson Gutierrez <david2021perez@outlook.es>
2025-04-22 14:22:36 -0500

    Update index, script, test; add shoe images and logo styles

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Hello");
+const logo = document.getElementById('logo');
+const filters = {
+  orange: 'hue-rotate(30deg)',
+  red: 'hue-rotate(0deg)',
+  purple: 'hue-rotate(280deg)'
+};
+
+const colors = ['orange', 'red', 'purple'];
+let current = 0;
+
+setInterval(() => {
+  const color = colors[current];
+  logo.style.filter = filters[color];
+  current = (current + 1) % colors.length;
+}, 1000);
